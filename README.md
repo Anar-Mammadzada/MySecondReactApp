b4e0f0e652e91bf790c5edf57a149cb014f74d77
Anar-Mammadzada <anarmammadza@gmail.com>
2024-02-28 16:16:44 +0400

    Add class based component and HemisphereDisplay file

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-This is my second simple react app. I used class based component and functional component.
+This is my second simple react app. I used class based component and functional component. This app will help us to the location of a user. If he/she is located in the 
+northern hemisphere or in the southern hemisphere.
---
+++ hemisphere/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HemisphereDisplay from './HemisphereDisplay';
+
+
+class App extends React.Component {
+    render() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => console.log(position),
+            (error) => console.log(error)
+        )
+        return (
+            <div>
+                You are in the northern hemisphere.
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+)
